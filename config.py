c4f75a7563ca9779931eebc98e5839a4de38d529
PropeReferio <lemuel.b.stevens@gmail.com>
2020-02-11 13:21:05 -0600

    Heroku Ready

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config():
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
     #For the database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
